7b0c3b7af0fbfdcaf72b48d57d96e58f31f3ef3a
malavikaa02 <malavikaa267@gmail.com>
2024-07-18 22:41:27 +0530

    Home Updated

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/Banner'
 import BestSellerBooks from './BestSellBooks'
 import FavBook from './FavBook'
 import PromoBanner from './PromoBanner'
+import OtherBooks from './OtherBooks'
 
 const home = () => {
   return (
@@ -11,6 +12,7 @@ const home = () => {
       <BestSellerBooks/>
       <FavBook/>
       <PromoBanner/>
+      <OtherBooks/>
     </div>
   )
 }
